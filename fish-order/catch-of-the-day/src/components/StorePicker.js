30a95945d2f553fb8398af232dbb0ce17a71e8dd
Brenda Long <brenda@studio6thave.com>
2018-01-17 08:18:13 -0600

    created first component

---
+++ fish-order/catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,13 @@
+//need react component
+import React from 'react';
+
+class StorePicker extends React.Component {
+
+    render() {
+        return <p>Hello</p>
+    }
+
+}
+
+// export the the component to be used other places
+export default StorePicker;
--- fish-order/catch-of-the-day/src/index.js
+++ fish-order/catch-of-the-day/src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { render } from 'react-dom';
 //allows import of only specific methods
 
+//demo 1
+/*
 class StorePicker extends React.Component {
 
     render() {
@@ -10,7 +12,19 @@ class StorePicker extends React.Component {
     }
 
 }
-
+*/
 // render below refers to import above
 // what to render and then where to render
+/*
+render(<StorePicker/>, document.getElementById('main'));
+*/
+
+// check out dev tools, see the component
+
+//now move component to own file. and import into this one
+// end demo 1
+
+//use relative path (don't want to look in node_modules)
+import StorePicker from './components/StorePicker';
+
 render(<StorePicker/>, document.getElementById('main'));
