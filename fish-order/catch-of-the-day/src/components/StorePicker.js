da4957d7ab6601c711e8f946d218186a4a01058f
Brenda Long <brenda@studio6thave.com>
2018-01-17 09:12:49 -0600

    added jsx to store component

--- fish-order/catch-of-the-day/src/components/StorePicker.js
+++ fish-order/catch-of-the-day/src/components/StorePicker.js
@@ -4,7 +4,18 @@ import React from 'react';
 class StorePicker extends React.Component {
 
     render() {
-        return <p>Hello</p>
+        // example without JSX
+        // return React.createElement('p', {className: 'Test'}, 'You look marvelous today');
+        
+        //give return parens allowing for multiple lines
+        //return only returns one element. May wrap in div
+        return (
+            <form className="store-selector">
+                <h2>Please Enter A Store</h2>
+                <input type="text" required placeholder="Store Name" />
+                <button type="submit">Visit Store</button>
+            </form>
+        )
     }
 
 }
